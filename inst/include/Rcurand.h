11f4c3006341643cb77ef6842117e20c882f53d8
Kashif Rasul <kashif.rasul@gmail.com>
2013-08-20 16:26:08 +0200

    white space

--- inst/include/Rcurand.h
+++ inst/include/Rcurand.h
@@ -10,53 +10,53 @@ public:
   {
     curandCreateGenerator(&gen_, (curandRngType_t)rng_type);
   }
-  
+
   int setPseudoRandomGeneratorSeed(unsigned long seed)
   {
     return curandSetPseudoRandomGeneratorSeed(gen_, seed);
   }
-  
+
   NumericVector generateUniform(size_t num)
   {
     NumericVector hostData(num);
-    
+
     cudaMalloc((void **)&devData_, num*sizeof(double));
     curandGenerateUniformDouble(gen_, devData_, num);
     cudaMemcpy(hostData.begin() , devData_ , num * sizeof(double), cudaMemcpyDeviceToHost);
     cudaFree(devData_);
-    
+
     return hostData;
   }
-  
+
   NumericVector generateNormal(size_t num, float mean=0.0f, float stddev=1.0f)
   {
     NumericVector hostData(num);
-    
+
     cudaMalloc((void **)&devData_, num*sizeof(double));
     curandGenerateNormalDouble(gen_, devData_, num, mean, stddev);
     cudaMemcpy(hostData.begin() , devData_ , num * sizeof(double), cudaMemcpyDeviceToHost);
     cudaFree(devData_);
-    
+
     return hostData;
   }
-  
+
   NumericVector generateLogNormal(size_t num, float mean=0.0f, float stddev=1.0f)
   {
     NumericVector hostData(num);
-    
+
     cudaMalloc((void **)&devData_, num*sizeof(double));
     curandGenerateLogNormalDouble(gen_, devData_, num, mean, stddev);
     cudaMemcpy(hostData.begin() , devData_ , num * sizeof(double), cudaMemcpyDeviceToHost);
     cudaFree(devData_);
-    
+
     return hostData;
   }
-  
+
   ~Rcurand()
   {
     curandDestroyGenerator(gen_);
   }
-  
+
 private:
   curandGenerator_t gen_;
   double *devData_;
--- src/Rcurand.cpp
+++ src/Rcurand.cpp
@@ -5,14 +5,14 @@ RCPP_MODULE(Rcurand_module) {
 
   class_<Rcurand>("Rcurand")
   .constructor<int>("Create new random number generator of type rng_type.")
-  
-  .method("setPseudoRandomGeneratorSeed", &Rcurand::setPseudoRandomGeneratorSeed, 
+
+  .method("setPseudoRandomGeneratorSeed", &Rcurand::setPseudoRandomGeneratorSeed,
     "Set the seed value of the pseudo-random number generator.")
-  .method("generateUniform", &Rcurand::generateUniform, 
+  .method("generateUniform", &Rcurand::generateUniform,
     "Generate n uniformly distributed doubles.")
-  .method("generateNormal", &Rcurand::generateNormal, 
+  .method("generateNormal", &Rcurand::generateNormal,
     "Generate n normally distributed doubles with mean and stddev.")
-  .method("generateLogNormal", &Rcurand::generateNormal, 
+  .method("generateLogNormal", &Rcurand::generateNormal,
     "Generate n log-normally distributed doubles with mean and stddev.")
   ;
 };
